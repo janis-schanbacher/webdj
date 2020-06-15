6b0557de9373cc2cadc4de54b881fa8aa7f3b2df
Janis <40525401+janis-schanbacher@users.noreply.github.com>
2020-06-15 09:50:09 +0200

    Add volume controlls and crossfader (#2)

---
+++ src/components/styles/Mixer.styles.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  background: #1C1C1C;
+  padding: 20px 0 10px 0;
+`;
+
+export const VolumeSliderWrapper = styled.div`
+  height: 150px;
+`;
+
+export const BalanceSliderWrapper = styled.div`
+  margin: 0 auto;
+  margin-top: 20px;
+  width: 120px;
+`;
