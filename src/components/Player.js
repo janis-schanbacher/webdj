4b2889170cb791b13c023f46b736911225eae1f5
Unknown <morice18@hotmail.de>
2020-07-04 17:25:08 +0200

    added Looper and BeatJumper component

--- src/components/Player.js
+++ src/components/Player.js
@@ -2,14 +2,13 @@ import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import { Button, Radio } from "antd";
 import { PlayCircleOutlined, PauseCircleOutlined, LeftCircleOutlined, RightCircleOutlined } from "@ant-design/icons";
+import BeatJumper from "./BeatJumper";
+import Looper from "./Looper";
 
 const Player = ({ audioContext, audioBuffer, volume, ready, offset, startInSync, setStartInSync, syncDelay, bpm }) => {
   const [bufferSource, setBufferSource] = useState(null);
   const [startedAt, setStartedAt] = useState(null);
   const [pausedAt, setPausedAt] = useState(null);
-  const [moveSelection, setMoveSelection] = useState(16);
-  const [loopSelection, setLoopSelection] = useState(16);
-  const [loopStartTime, setLoopStartTime] = useState();
   const [loop, setLoop] = useState(false);
 
   const createGainNode = () => {
@@ -51,54 +50,6 @@ const Player = ({ audioContext, audioBuffer, volume, ready, offset, startInSync,
     }
   }, [startInSync]);
 
-  useEffect(() => {
-    if (loop && startedAt && bufferSource) {
-      const loopTime = loopSelection * (60 / bpm);
-      const current = Date.now() - startedAt;
-
-      bufferSource.loop = true;
-      bufferSource.loopStart = current / 1000;
-      bufferSource.loopEnd = (current / 1000) + loopTime;
-      setLoopStartTime(Date.now());
-    } else if (!loop && startedAt && bufferSource) {
-      const timePassed = Date.now() - loopStartTime;
-      const loopTime = loopSelection * (60 / bpm) * 1000;
-      setStartedAt(startedAt + (Math.floor(timePassed / loopTime) * loopTime));
-      bufferSource.loop = false;
-    }
-  }, [loop]);
-
-  const beatJump = (type) => {
-    if (bufferSource) {
-      const paused = Date.now() - startedAt;
-      const timeToAdd = moveSelection * (60 / bpm);
-
-      if (
-        (type === 2 && (paused / 1000 + timeToAdd) > bufferSource.buffer.duration)
-        || (type === 1 && (paused / 1000 - timeToAdd) < 0)
-      ) {
-        return;
-      }
-
-      bufferSource.stop();
-
-      const source = audioContext.createBufferSource();
-      setBufferSource(source);
-      source.buffer = audioBuffer;
-      source.connect(gainNode);
-
-      if (type === 2) {
-        source.start(0, paused / 1000 + timeToAdd);
-        console.log(paused / 1000 + timeToAdd);
-        setStartedAt(startedAt - (timeToAdd * 1000));
-      } else {
-        source.start(0, paused / 1000 - timeToAdd);
-        console.log(paused / 1000 - timeToAdd);
-        setStartedAt(startedAt + (timeToAdd * 1000));
-      }
-    }
-  };
-
   /**
    * Play from start or if paused resume from last position
    */
@@ -131,33 +82,25 @@ const Player = ({ audioContext, audioBuffer, volume, ready, offset, startInSync,
 
   return (
     <div>
-      <div>
-        <Radio.Group value={moveSelection} onChange={e => setMoveSelection(e.target.value)}>
-          <Radio.Button value={1}>1</Radio.Button>
-          <Radio.Button value={2}>2</Radio.Button>
-          <Radio.Button value={4}>4</Radio.Button>
-          <Radio.Button value={8}>8</Radio.Button>
-          <Radio.Button value={16}>16</Radio.Button>
-          <Radio.Button value={32}>32</Radio.Button>
-        </Radio.Group>
-        <Button disabled={loop || !startedAt} onClick={() => beatJump(1)}>
-          <LeftCircleOutlined/>
-        </Button>
-        <Button disabled={loop || !startedAt} onClick={() => beatJump(2)}>
-          <RightCircleOutlined/>
-        </Button>
-      </div>
-      <div>
-        <Radio.Group value={loopSelection} onChange={e => setLoopSelection(e.target.value)}>
-          <Radio.Button disabled={loop} value={1}>1</Radio.Button>
-          <Radio.Button disabled={loop} value={2}>2</Radio.Button>
-          <Radio.Button disabled={loop} value={4}>4</Radio.Button>
-          <Radio.Button disabled={loop} value={8}>8</Radio.Button>
-          <Radio.Button disabled={loop} value={16}>16</Radio.Button>
-          <Radio.Button disabled={loop} value={32}>32</Radio.Button>
-        </Radio.Group>
-        <Button disabled={!startedAt} onClick={() => setLoop(!loop)} danger={loop}>Loop</Button>
-      </div>
+      <BeatJumper
+        bufferSource={bufferSource}
+        setBufferSource={setBufferSource}
+        startedAt={startedAt}
+        setStartedAt={setStartedAt}
+        bpm={bpm}
+        gainNode={gainNode}
+        audioBuffer={audioBuffer}
+        audioContext={audioContext}
+        loop={loop}
+      />
+      <Looper
+        loop={loop}
+        setLoop={setLoop}
+        bpm={bpm}
+        bufferSource={bufferSource}
+        setStartedAt={setStartedAt}
+        startedAt={startedAt}
+      />
       <Button disabled={!ready} onClick={play}>
         <PlayCircleOutlined />
       </Button>
@@ -173,9 +116,9 @@ Player.propTypes = {
   audioBuffer: PropTypes.object,
   volume: PropTypes.number,
   ready: PropTypes.bool.isRequired,
-  offset: PropTypes.number,
-  startInSync: PropTypes.bool,
-  setStartInSync: PropTypes.func,
+  offset: PropTypes.number.isRequired,
+  startInSync: PropTypes.bool.isRequired,
+  setStartInSync: PropTypes.func.isRequired,
 };
 
 Player.defaultProps = {
